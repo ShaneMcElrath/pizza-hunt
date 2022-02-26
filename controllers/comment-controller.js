e169b37f1eb7d0cfc920f810f417630ad1a3e134
Shane McElrath <shane@rangermac.com>
2022-02-26 17:34:23 -0500

    Added models for comments. Added controller functions for comments. Created routes for comments. Added fetch for all pizza data to display one home page.

---
+++ controllers/comment-controller.js
@@ -0,0 +1,52 @@
+const { Comment, Pizza } = require('../models');
+
+const commentController = {
+  // add comment to pizza
+  addComment({ params, body }, res) {
+    console.log(body);
+    Comment.create(body)
+      .then(({ _id }) => {
+        return Pizza.findOneAndUpdate(
+          { _id: params.pizzaId },
+          { $push: { comments: _id } },
+          { new: true }
+        );
+      })
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => res.json(err));
+  },
+
+  // remove comment
+  removeComment({ params }, res) {
+    Comment.findOneAndDelete({ _id: params.commentId })
+      .then(function(deletedComment) {
+                
+        if (!deletedComment) {
+          throw new Error('No comment with this id!');
+        }
+
+        return Pizza.findOneAndUpdate(
+          { _id: params.pizzaId },
+          { $pull: { comments: params.commentId } },
+          { new: true }
+        );
+      })
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          throw new Error('No pizza found with this id!');
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => {
+        res.status(404).json({ message: err.message });
+      });
+  }
+};
+
+module.exports = commentController;
